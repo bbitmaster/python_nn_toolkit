796f58853b5e4a2c5645e26b51b4d4a6e87d5a0a
Benjamin Fredrick Goodrich <bgoodric@hydra7.eecs.utk.edu>
2013-05-15 23:12:19 -0400

    added linear rectifier activation. Improved tests

--- test_random.py
+++ test_random.py
@@ -1,20 +1,29 @@
+import time
 import numpy as np;
 
-import nnet_toolkit;
+from nnet_toolkit import nnet_toolkit;
 
-layers = [nnet_toolkit.layer(2),nnet_toolkit.layer(32),nnet_toolkit.layer(1)];
+#layers = [nnet_toolkit.layer(2),nnet_toolkit.layer(128,'squash'),nnet_toolkit.layer(1,'squash')];
+layers = [nnet_toolkit.layer(400),nnet_toolkit.layer(128,'squash'),nnet_toolkit.layer(1,'squash')];
 
-training_data = np.array([[0,0,1,1],[0,1,0,1]]);
-training_out = np.array([0,1,1,0]);
+#training_data = np.array([[0,0,1,1],[0,1,0,1]]);
+#training_out = np.array([0,1,1,0]);
 
-net = nnet_toolkit.net(layers);
+training_data = np.random.random((400,500));
+training_out = np.random.random((1,500));
+
+net = nnet_toolkit.net(layers,step_size=.1);
 
 net.input = training_data;
-for i in range(1000000):
+t = time.time();
+for i in range(100000):
 	net.feed_forward();
 	net.error = net.output - training_out;
 	net.back_propagate();
 	net.update_weights();
 	if(i%1000 == 0):
-		print(str(net.error));
+		#print("iteration: " + str(i) + " " + str(net.error));
+		t_delta = time.time() - t;
+		print("iteration: " + str(i) + " " + str(t_delta))
+		t = time.time();
 
