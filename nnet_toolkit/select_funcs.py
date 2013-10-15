795171e1802541ed8577aa6fa1b33b32f2f3a060
Ben Goodrich <bbitmaster@gmail.com>
2013-10-14 20:33:31 -0400

    added most positive selection function

--- nnet_toolkit/select_funcs.py
+++ nnet_toolkit/select_funcs.py
@@ -38,12 +38,22 @@ def most_negative_select_func(self,params):
     self.selected_neurons = activation >= sorted_activations[n_active_count,:]
     self.output[self.selected_neurons] = 0;
 
+#select based on most positive activation. (selects the smallest activation where smallest)
+#is defined as the lowest real value)
+def most_positive_select_func(self,params):
+    n_active_count = params;
+    activation = self.weighted_sums
+    #place smallest activations in top rows
+    sorted_activations = np.sort(activation,axis=0)
+    #select the n'th smallest activation, and set everything >= it to 0
+    self.selected_neurons = activation < sorted_activations[-n_active_count,:]
+    self.output[self.selected_neurons] = 0;
+
 #select based on minimum euclidean distance to dot product=0 hyperplane. 
 def minabs_select_func_normalized(self,params):
     n_active_count = params;
     #get the norm of each neurons weights.
     weight_norm = np.sum(self.weights**2,axis=1)**(1./2.);
-
     #need to do a row-wise divide (the transposes handle that)
     #each activation needs to be divided by the norm of that neuron's weights    
     normalized_activations = (self.weighted_sums.T/weight_norm.T).T
@@ -60,7 +70,6 @@ def maxabs_select_func_normalized(self,params):
     n_active_count = params;
     #get the norm of each neurons weights.
     weight_norm = np.sum(self.weights**2,axis=1)**(1./2.);
-
     #need to do a row-wise divide (the transposes handle that)
     #each activation needs to be divided by the norm of that neuron's weights    
     normalized_activations = (self.weighted_sums.T/weight_norm.T).T
@@ -78,7 +87,6 @@ def most_negative_select_func_normalized(self,params):
     n_active_count = params;
     #get the norm of each neurons weights.
     weight_norm = np.sum(self.weights**2,axis=1)**(1./2.);
-
     #need to do a row-wise divide (the transposes handle that)
     #each activation needs to be divided by the norm of that neuron's weights    
     normalized_activations = (self.weighted_sums.T/weight_norm.T).T
@@ -87,4 +95,18 @@ def most_negative_select_func_normalized(self,params):
     sorted_activations = np.sort(activation,axis=0)
     #select the n'th smallest activation, and set everything >= it to 0
     self.selected_neurons = activation >= sorted_activations[n_active_count,:]
+    self.output[self.selected_neurons] = 0;
+    
+def most_positive_select_func_normalized(self,params):
+    n_active_count = params;
+    #get the norm of each neurons weights.
+    weight_norm = np.sum(self.weights**2,axis=1)**(1./2.);
+    #need to do a row-wise divide (the transposes handle that)
+    #each activation needs to be divided by the norm of that neuron's weights    
+    normalized_activations = (self.weighted_sums.T/weight_norm.T).T
+    activation = normalized_activations
+    #place smallest activations in top rows
+    sorted_activations = np.sort(activation,axis=0)
+    #select the n'th smallest activation, and set everything >= it to 0
+    self.selected_neurons = activation < sorted_activations[-n_active_count,:]
     self.output[self.selected_neurons] = 0;
